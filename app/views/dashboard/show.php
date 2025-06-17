fc59bc1dfdb95587443b56205c0d50d7179b12be
flaxd <flaviaduarte600@gmail.com>
2025-06-16 21:51:00 -0300

    Delete app/views directory

--- app/views/dashboard/show.php
+++
@@ -1,7 +0,0 @@
-    <h1>Detalles de Usuario</h1>
-    <p>ID de usuario: <?= $userId ?? 'N/A' ?></p>
-    <p>Nombre: <?= htmlspecialchars($user['Nombre']) ?></p>
-    <p>Email: <?= htmlspecialchars($user['Correo']) ?></p>
-    
-    <p><a href="/dashboard">← Volver al dashboard</a></p>
-
